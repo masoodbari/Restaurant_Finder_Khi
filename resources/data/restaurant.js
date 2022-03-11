97789cd7eceb4f1f6644d91fc68e133419cc4774
masoodbari <masoodbari@hotmail.com>
2022-03-11 14:21:55 +0400

    Restaurant_Finder
    Restaurant

---
+++ resources/data/restaurant.js
@@ -0,0 +1,89 @@
+var restaurantList = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0614712, 24.8081254]
+    },
+    "properties": {
+      "name": "Espresso Cafe",
+      "address": "Khayaban e Shahbaz, Karachi,Pakistan",
+      "phone": "---------"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0443407, 24.7971902]
+    },
+    "properties": {
+      "name": "Daily dubai resturant",
+      "address": "Badar Commercial 10th Street,DHA Phase 5, Karachi,Pakistan",
+      "phone": "--------"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0393296, 24.8063725]
+    },
+    "properties": {
+      "name": "Movenpick Ice-cream Parl",
+      "address": "26th Street,DHA Phase 5, Karachi Pakistan",
+      "phone": "---------"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [ 67.0694223, 24.8095767]
+    },
+    "properties": {
+      "name": "Hot 'n' Spicy",
+      "address": "DHA Phase 6,Karachi,Pakistan",
+      "phone": "---------"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0485463, 24.8135863]
+    },
+    "properties": {
+      "name": "One Potato Two Potato",
+      "address": "Stadium Lane 4,DHA Phase 5, Karachi,Pakistan",
+      "phone": "---------"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0506747, 24.8123479]
+    },
+    "properties": {
+      "name": "Agha Juice",
+      "address": "Khayaban e Muhafiz,DHA Phase 5, Karachi,Pakistan",
+      "phone": "---------"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [67.0483971, 24.7944601]
+    },
+    "properties": {
+      "name": "Hardees",
+      "address": "Khayaban-e-Shahbaz,DHA Phase 5, Karachi,Pakistan",
+      "phone": "---------"
+    }
+  }
+]
+
